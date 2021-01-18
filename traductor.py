ccef3e2ac4d6b2f3fd80d2ae57023e9e4b77062b
Luis Camilo <lferreira2017@alu.uct.cl>
2021-01-18 16:28:01 -0300

    Comienzo con una interfaz simple y comienzo con la traduccion de oraciones

--- traductor.py
+++ traductor.py
@@ -1,37 +1,61 @@
-#import es_core_news_sm
-#nlp = es_core_news_sm.load()
-#doc = nlp("El kenai traga pico")
-#print([(w.text, w.pos_) for w in doc])
-
-
+import es_core_news_sm
 import json 
 
+def BuscarTraduccion(buscar):
 
-run = True
-while(run):
 
+
+	#Abre el archivo json
 	with open('json/dic.json') as file:
+		
+		#Carga el archivo json
 		data = json.load(file)
-
-		buscar  = input("Ingresa una palabra: ")
+		
 
 		for letra in data['Palabras']:
+			
+			#Recorre el json de manera alfabetica
 			for pal in data['Palabras'][letra]:
-				palabra = pal[0]
-				significado = pal[1][1:]
+				palabra = pal[0] #Se guarda la palabra 
+				significado = pal[1][1:] # Se guarda su significado
 
-				if (significado.find(",") > 1):
+				if (significado.find(",") > 0):
 					sig = significado.split(",")
-					
+					#print(sig)
+					#print(sig)				
 					for s in sig:
+						#print(sig)
 						if(s.strip().lower() == buscar.lower()):
-							print(palabra)
-
-
+							#print(sig)
+							return palabra
 				else:
 					#print(significado)
 					if(significado.lower() == buscar.lower()):
-						print("Traduccion : ",palabra)
+						return palabra
+						
 
-		if(buscar == "fin"):
-			run = False
+#nlp = es_core_news_sm.load()
+#doc = nlp("yo quiero caminar , pero no puedo")
+#print([(w.text, w.pos_) for w in doc])
+
+correr = True
+while (correr):
+
+	nlp = es_core_news_sm.load()
+	text = input("Ingresa oracion : ")
+	buscar = nlp(text)
+
+
+	t = " "
+	for w in buscar:
+		print (w.text)
+		if(BuscarTraduccion(w.text) != None):
+			t += BuscarTraduccion(w.text)+" "
+		else : 
+			print (w.text + ", No existe ! ")
+	print("Traduccion : ",t)
+
+	#print(BuscarTraduccion(buscar))
+
+	if(text == "fin"):
+		correr = False
